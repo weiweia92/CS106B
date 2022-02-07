82431aea05fd3869b33a5f1723200b15488f59ac
weiweia92 <weiweia92@163.com>
2022-02-07 11:33:00 +0800

    .

---
+++ Assignment 0/build/CMakeFiles/nameHash.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/nameHash.dir/nameHash.cpp.o"
+  "CMakeFiles/nameHash.dir/nameHash.cpp.o.d"
+  "nameHash"
+  "nameHash.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/nameHash.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ Assignment 0/build/CMakeFiles/nameHash.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for nameHash.
+# This may be replaced when dependencies are built.
